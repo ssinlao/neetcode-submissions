fcfc48e3b20b490b68bfafe1e613311f4cd47324
Stella Sinlao <114186529+ssinlao@users.noreply.github.com>
2026-09-23 15:15:57 -0700

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {} # occurences of each char
+        result = 0
+
+        l = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0) # count[char at right ptr], if char doesnt exist return 0
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+            result = max(result, r - l + 1)
+        return result
