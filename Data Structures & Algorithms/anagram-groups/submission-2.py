e708179dc264830820536897aaee6b893730aad4
Stella Sinlao <114186529+ssinlao@users.noreply.github.com>
2026-09-23 16:34:35 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # practice speed round
+
+        result = defaultdict(list)
+        for s in strs:
+            count = [0] * 26 # code maker that words will be matched to
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            
+            result[tuple(count)].append(s)
+        return list(result.values())
+
+        # key things to remember
+        # use ascii val in count array
+        # need to cast tuple to append to result dict
+        # need to cast list to return values
